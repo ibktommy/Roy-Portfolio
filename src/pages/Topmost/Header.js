e19c6448f50464c11d3847042f95deb605558c42
ibktommy <atomquartz@gmail.com>
2022-08-24 13:08:36 +0100

    Work on displaying, structuring and styling of the Header section of Topmost-Page

--- src/pages/Topmost/Header.js
+++ src/pages/Topmost/Header.js
@@ -8,9 +8,18 @@ const Header = () => {
         I overtook the task of redesigning their web platform and helping to solve problems faced by its users.
       </header>
 
-      <div className="header-details">
+      <div className="header-details flex">
         <div className="header-details_role">
-          
+          <h4>Product Designer</h4>
+          <p>Webflow Developer</p>
+        </div>
+        <div className="header-details_duration">
+          <h4>Duration</h4>
+          <p>May 2022- August 2022</p>
+        </div>
+        <div className="header-details_platform">
+          <h4>Platform</h4>
+          <p>Responsive Website</p>
         </div>
       </div>
     </section>
