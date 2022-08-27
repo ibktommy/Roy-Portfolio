29ec9e8cfecdc7f49d77f64632cbd590ff7f882b
ibktommy <atomquartz@gmail.com>
2022-08-27 21:58:11 +0100

    Done with structuring and styling visual-design component

--- src/pages/Isave/VisualDesign.js
+++ src/pages/Isave/VisualDesign.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import VisualDesignCard from '../../components/VisualDesignCard'
 import visualDesignData from './VisualDesignData.json'
+import visualmageOne from '../../images/isave/main/Earn- flow1.png'
+import visualmageTwo from '../../images/isave/main/Earn- flow2.png'
+import visualmageThree from '../../images/isave/main/Earn- flow3.png'
+import visualmageFour from '../../images/isave/main/successful.png'
 
 const VisualDesign = () => {
   return (
     <div className="isave-visual">
       
       <h4>Visual Design</h4>
+
       {visualDesignData.map(({ id, ...props }) => {
         return <VisualDesignCard key={id} {...props}/>
       })}
+
+      <div className="image-container flex">
+        <img src={visualmageOne} alt="visual-img" />
+        <img src={visualmageTwo} alt="visual-img" />
+        <img src={visualmageThree} alt="visual-img" />
+        <img src={visualmageFour} alt="visual-img" />
+      </div>
       
     </div>
   )
