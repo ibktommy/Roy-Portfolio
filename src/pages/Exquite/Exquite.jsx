1ea41c61c4dfc1613b017905506db83fd007ebc3
ibktommy <atomquartz@gmail.com>
2022-08-30 17:39:46 +0100

    Create file structure for Exquite Page

--- src/pages/Exquite/Exquite.jsx
+++ src/pages/Exquite/Exquite.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import './Exquite.scss'
+import {
+	Header,
+	About,
+	Statement,
+	Goals,
+	UserFlow,
+	Wireframe,
+	VisualDesign,
+} from "../Exquite";
+import Footer from "../../components/Footer";
 
 const Exquite = () => {
-	return <div>Exquite</div>;
+	return (
+		<section className="exquite">
+			<Header />
+			<Footer children={"exquite autos"} />
+		</section>
+	);
 };
 
 export default Exquite;
