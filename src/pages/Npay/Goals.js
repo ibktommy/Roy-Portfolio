2e28a00b7b23beed1b3fda6fabc5643590577d69
ibktommy <atomquartz@gmail.com>
2022-09-06 11:27:56 +0100

    Added elements and styling to the Goals section of the Npay Page

---
+++ src/pages/Npay/Goals.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Goals = () => {
+  return (
+    <div className="npay-goals">
+      <h4>Goals</h4>
+      <p>
+        <b>Provide seamless user experience</b> <br />
+        Ensuring good user exprience by simplifying the app and its payments system.
+      </p>
+      <p>
+        <b>Drive confidence </b> <br />
+        Solve the problem of lack of confidence in Nigeria’s payment platforms and driving popularity of the app.
+      </p>
+      <p>
+        <b>Encourage tax payers to the app</b> <br />
+        The primary targets of the app, which are taxpayers should be driven to download and use the app as first choice platform for settling bills.
+      </p>
+    </div>
+  )
+}
+
+export default Goals
--- src/pages/Npay/Npay.jsx
+++ src/pages/Npay/Npay.jsx
@@ -19,6 +19,7 @@ const Npay = () => {
 			<Header />
 			<Background />
 			<Statement />
+			<Goals />
 		</section>
 	);
 };
