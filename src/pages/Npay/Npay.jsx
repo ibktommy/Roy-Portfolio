19a36d59700ebae29d89dc61a7ae99273d42c0e9
ibktommy <atomquartz@gmail.com>
2022-09-06 10:54:19 +0100

    Selecting Images to be used for Npay Page

--- src/pages/Npay/Npay.jsx
+++ src/pages/Npay/Npay.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import {
+	Header,
+	Background,
+	Color,
+	Design,
+	Goals,
+	Statement,
+	UserFlow,
+	VisualDesign,
+	Wireframe,
+} from "../Npay";
 
 const Npay = () => {
 	return <div>Npay</div>;
