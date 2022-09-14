6f02a32c7855d3eb09afff29e3bd59c5f631d371
ibktommy <atomquartz@gmail.com>
2022-09-14 15:20:23 +0100

    Restyling the project-summary area of the Home Page to make it look good

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const HomeCard = ({ title, details, image, btnText, reverse, tag, link }) => {
-  // Function to Make Each Page Begin From Top whenbutton is clicked
+  // Function to Make Each Page Begin From Top when button is clicked
   const hideModal = () => {
     window.scrollTo({
       top: 0,
-      // behavior: 'smooth'
     })
   }
 
