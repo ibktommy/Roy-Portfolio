3d1abed8644e4f1daa391d78db4da5a28546b644
ibktommy <atomquartz@gmail.com>
2022-09-16 21:50:16 +0100

    Styling of the contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,42 +6,43 @@ const Contact = () => {
 		<section className="contact">
 			<h2 className="title">Contact Me</h2>
 
-			<p>
+			<p className="para">
 				I am available for any official, freelance and contract works. Do drop
 				your message and I will reach back to you almost immediately. Gracias!
 				&#128526; &#9996;
 			</p>
 
 			<div className="contact-details">
-				<form>
-					<div className="contact-details_form-card">
+				<form className="form row-flex">
+					<div className="form-card">
 						<input type="text" placeholder="First Name" />
 						<input type="text" placeholder="Last Name" />
 					</div>
-					<div className="contact-details_form-card">
+					<div className="form-card">
 						<input type="email" placeholder="Email" />
 						<input type="text" placeholder="Subject" />
 					</div>
 					<textarea placeholder="Message"></textarea>
+					<button type="submit">Send</button>
 				</form>
 
-				<div className="contact-details-info">
+				<div className="contact-details-info row-flex">
 					<div className="contact-details-info_card">
-						<i></i>
+						<i className="fa-solid fa-location-dot"></i>
 						<div className="contact-details-info_card-para">
 							<h5>Address</h5>
 							<p>Lagos, Nigeria.</p>
 						</div>
 					</div>
 					<div className="contact-details-info_card">
-						<i></i>
+						<i className="fa-solid fa-at"></i>
 						<div className="contact-details-info_card-para">
 							<h5>Email</h5>
 							<p>roy@gmail.com</p>
 						</div>
 					</div>
 					<div className="contact-details-info_card">
-						<i></i>
+						<i className="fa-solid fa-phone"></i>
 						<div className="contact-details-info_card-para">
 							<h5>Mobile</h5>
 							<p>+234 906 890 3908</p>
