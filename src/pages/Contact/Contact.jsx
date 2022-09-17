65137ffdb451221c2158512aaee43483d5f8d2c2
ibktommy <atomquartz@gmail.com>
2022-09-17 15:55:28 +0100

    Send Form-Inputs to FormCarry Server

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,57 @@
 import React, { useRef, useState } from "react";
 import "./Contact.scss";
+import axios from "axios";
 import FormImage from "../../images/logo/thanks.gif";
 
 // Formspree Key
 const formSpreeKey = process.env.REACT_APP_FORMSPREE_KEY;
+const url = `https://formcarry.com/s/${formSpreeKey}`;
 
 const Contact = () => {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [subject, setSubject] = useState("");
+	const [message, setMessage] = useState("");
+
+	// Function to Submit Form
+	const formSubmitHandler = async (e) => {
+		e.preventDefault();
+
+		if (
+			firstName === "" ||
+			lastName === "" ||
+			email === "" ||
+			message === "" ||
+			subject === ""
+		) {
+			alert("PLEASE INPUT ALL FORM FIELDS!");
+		} else {
+			axios({
+				url,
+				method: "post",
+				headers: {
+					Accept: "application.json",
+				},
+				data: {
+					FirstName: firstName,
+					LastName: lastName,
+					Email: email,
+					Subject: subject,
+					Message: message,
+				},
+			})
+				.then((response) => {
+					if (response.status === 200) {
+						alert("THANKS, YOUR MESSAGE HAS BEEN SENT");
+					}
+				})
+				.catch((error) => {
+					alert("OOPS! SOMETHING WENT WRONG!");
+				});
+		}
+	};
+
 	return (
 		<section className="contact animate__animated animate__zoomIn">
 			<h2 className="title">Contact Me</h2>
@@ -17,31 +63,54 @@ const Contact = () => {
 			</p>
 
 			<div className="contact-details">
-				<form method="POST" className="form row-flex">
+				<form
+					method="POST"
+					onSubmit={formSubmitHandler}
+					className="form row-flex"
+				>
 					<div className="form-card">
 						<input
 							id="first-name"
 							name="First Name"
 							type="text"
 							placeholder="First Name"
+							value={firstName}
+							onChange={(e) => setFirstName(e.target.value)}
 						/>
 						<input
 							id="last-name"
 							name="Last Name"
 							type="text"
 							placeholder="Last Name"
+							value={lastName}
+							onChange={(e) => setLastName(e.target.value)}
 						/>
 					</div>
 					<div className="form-card">
-						<input id="email" name="email" type="email" placeholder="Email" />
+						<input
+							id="email"
+							name="email"
+							type="email"
+							placeholder="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
 						<input
 							id="subject"
 							name="subject"
 							type="text"
 							placeholder="Subject"
+							value={subject}
+							onChange={(e) => setSubject(e.target.value)}
 						/>
 					</div>
-					<textarea id="message" name="messsage" placeholder="Message" />
+					<textarea
+						id="message"
+						name="messsage"
+						placeholder="Message"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+					/>
 					<button type="submit">Send</button>
 				</form>
 
