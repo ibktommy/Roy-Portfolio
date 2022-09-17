2943b79b05a04bd5de816c777d1006362eeb2bfb
ibktommy <atomquartz@gmail.com>
2022-09-17 08:29:34 +0100

    Install Formspree dependecy and setup the contact-page-form with formspree so that users form-inputs will be sent to the client

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,29 +1,73 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import "./Contact.scss";
+import FormImage from "../../images/logo/thanks.gif";
+
+// Formspree Key
+const formSpreeKey = process.env.REACT_APP_FORMSPREE_KEY;
 
 const Contact = () => {
+	// Form State
+	const [state, handleSubmit] = useForm(`${formSpreeKey}`);
+	if (state.succeeded) {
+		return (
+			<div className="form-success animate__animated animate__zoomInDown">
+				<p className="form-text">
+					Your Message as been sent, I will respond shortly. Thanks!
+				</p>
+				<img src={FormImage} alt="formgif" />
+			</div>
+		);
+	}
+
 	return (
-		<section className="contact">
+		<section className="contact animate__animated animate__fadeInDown">
 			<h2 className="title">Contact Me</h2>
 
 			<p className="para">
 				I am available for any official, freelance and contract works. Do drop
 				your message and I will reach back to you almost immediately. Gracias!
-				&#128526; &#9996;
+				&#128526;
 			</p>
 
 			<div className="contact-details">
-				<form className="form row-flex">
+				<form className="form row-flex" onSubmit={handleSubmit}>
 					<div className="form-card">
-						<input type="text" placeholder="First Name" />
-						<input type="text" placeholder="Last Name" />
+						<input
+							id="first-name"
+							name="First Name"
+							type="text"
+							placeholder="First Name"
+						/>
+						<input
+							id="last-name"
+							name="Last Name"
+							type="text"
+							placeholder="Last Name"
+						/>
 					</div>
 					<div className="form-card">
-						<input type="email" placeholder="Email" />
-						<input type="text" placeholder="Subject" />
+						<input id="email" name="email" type="email" placeholder="Email" />
+						<ValidationError
+							prefix="Email"
+							field="email"
+							errors={state.errors}
+						/>
+						<input
+							id="subject"
+							name="subject"
+							type="text"
+							placeholder="Subject"
+						/>
 					</div>
-					<textarea placeholder="Message"></textarea>
-					<button type="submit">Send</button>
+					<textarea
+						id="message"
+						name="messsage"
+						placeholder="Message"
+					></textarea>
+					<button type="submit" disabled={state.submitting}>
+						Send
+					</button>
 				</form>
 
 				<div className="contact-details-info row-flex">
@@ -38,7 +82,7 @@ const Contact = () => {
 						<i className="fa-solid fa-at"></i>
 						<div className="contact-details-info_card-para">
 							<h5>Email</h5>
-							<p>roy@gmail.com</p>
+							<p>ayodejiroy@gmail.com</p>
 						</div>
 					</div>
 					<div className="contact-details-info_card">
